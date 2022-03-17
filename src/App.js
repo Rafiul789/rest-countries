48eea71313bd449320de25d009015c50481229b0
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-17 16:33:45 +0600

    more component added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { useEffect,useState } from 'react';
 import Countries from './components/Countries/Countries';
-import Person from './components/Countries/Person/Person';
-import Header from './components/Countries/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-    <Countries> </Countries> 
-    <Person> </Person>
-    <Header> </Header>
-    </div>
-  );
-}
-// function LoadCountries(){
-//   const [countries,setCountries]=useState([])
-//   useEffect(()=>{fetch('https://restcountries.com/v3.1/all') .then(res=>res.json()) .then(data=>setCountries(data))   },[])
-//   return(
-//     <div> 
-//       <h1>Visiting every country of the world!!!</h1>
-//       <h3>Available Countries:{countries.length}</h3>
-//       {
-//         countries.map(country=><Country name={country.name.common} population={country.population}  />)
-//       }
-//        </div>
-//   )
-// }
-// function Country(props){
-//   return ( <div> <h2>Name:{props.name}</h2> <h4>Population:{props.population}</h4>  </div>)
-// }
-export default App;
+    <div className="App"> <Countries></Countries>  </div>
+  )
+};
